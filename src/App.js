14853098fd152b42f9dfebca17e3d3615947ece8
Ekta Goyal <ektagoyal@Ektas-MacBook-Pro.local>
2020-02-21 11:38:14 +0530

    basic react program

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "luna",
+      animal: "dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "pepper",
+      animal: "bird",
+      breed: "cocktail"
+    }),
+    React.createElement(Pet, {
+      name: "doink",
+      animal: "cat",
+      breed: "mixed"
+    })
+  ]);
+};
+
+render(React.createElement(App), document.getElementById("root"));
